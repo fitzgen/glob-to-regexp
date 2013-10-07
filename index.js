cb0b0a5aa56b1c9d1fa50870564e01ba29bddcfa
Larry Battle <larry.battle.tech@gmail.com>
2013-10-07 14:55:36 -0500

    Update index.js
    Converted tabs to 2 spaces.

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 module.exports = function (glob) {
-	if (glob === null || typeof glob === "undefined") {
-		return null;
-	}
-	var str = String(glob), 
-	  reStr = "", 
-	  c;
-	  
-	  
-	for (var i = 0, len = str.length; i < len; i++) {
-		c = str[i];
-		switch (c) {
-		case "\\":
-		case "/":
-		case "$":
-		case "^":
-		case "+":
-		case "?":
-		case ".":
-		case "(":
-		case ")":
-		case "=":
-		case "!":
-		case "|":
-		case "{":
-		case "}":
-		case ",":
-		case "[":
-		case "]":
-			reStr += "\\" + c;
-			break;
-		case "*":
-			reStr += ".*";
-			break;
-		default:
-			reStr += c;
-		}
-	}
-	return new RegExp("^" + reStr + "$");
+  if (glob === null || typeof glob === "undefined") {
+    return null;
+  }
+  var str = String(glob),
+  reStr = "",
+  c;
+  
+  for (var i = 0, len = str.length; i < len; i++) {
+    c = str[i];
+    switch (c) {
+    case "\\":
+    case "/":
+    case "$":
+    case "^":
+    case "+":
+    case "?":
+    case ".":
+    case "(":
+    case ")":
+    case "=":
+    case "!":
+    case "|":
+    case "{":
+    case "}":
+    case ",":
+    case "[":
+    case "]":
+      reStr += "\\" + c;
+      break;
+    case "*":
+      reStr += ".*";
+      break;
+    default:
+      reStr += c;
+    }
+  }
+  return new RegExp("^" + reStr + "$");
 };
